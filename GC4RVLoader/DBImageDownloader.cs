ea4300d1168d40265f5a132caadd340f2c34831f
Jonathan Day <j.day151r@gmail.com>
2022-11-13 12:22:23 -0800

    main files

---
+++ GC4RVLoader/DBImageDownloader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace GC4RVLoader
+{
+    public class DBImageDownloader
+    {
+        private const string baseUrl = "https://art.gametdb.com";
+        private const string platform = "wii";
+        private const string coverType = "cover";
+        private const string region = "US";
+        private const string imageFormat = "png";
+
+        public static void DownloadCover(string gameId)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string sep = $"{Path.DirectorySeparatorChar}";
+                string destinationDirectory = $"{Directory.GetCurrentDirectory()}{sep}rvloader{sep}covers";
+                Directory.CreateDirectory(destinationDirectory);
+                string fileName = $"{destinationDirectory}{sep}{gameId}.{imageFormat}";
+                Console.Write($"Downloaded cover for {gameId}...");
+                try
+                {
+                    client.DownloadFile(new Uri(BuildDownloadUrl(gameId)), fileName);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error! {e.Message}");
+                    return;
+                }
+                Console.WriteLine("Done");
+            }
+        }
+
+        public static void DownloadCovers(string[] gameIds)
+        {
+            foreach (string id in gameIds)
+            {
+                DownloadCover(id);
+            }
+        }
+
+        private static string BuildDownloadUrl(string gameId)
+        {
+            return $"{baseUrl}/{platform}/{coverType}/{region}/{gameId}.{imageFormat}";
+        }
+    }
+}
---
+++ GC4RVLoader/GameDirectoryFormatter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace GC4RVLoader
+{
+    public class GameDirectoryFormatter
+    {
+        public static void MoveGameToNestedFolder(string filePath)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(filePath);
+            string destinationPath = $"{GetBaseGameDirectory()}{Path.DirectorySeparatorChar}{fileName}";
+            
+            Directory.CreateDirectory(destinationPath);
+            string destinationFile = $"{destinationPath}{Path.DirectorySeparatorChar}game.iso";
+            File.Move(filePath, destinationFile);
+            Console.WriteLine($"Game moved to destination: {destinationFile}");
+        }
+
+        public static void MoveGamesToNestedFolder(string[] filePaths)
+        {
+            foreach (string path in filePaths)
+            {
+                MoveGameToNestedFolder(path);
+            }
+        }
+
+        public static string GetBaseGameDirectory()
+        {
+            return $"{Directory.GetCurrentDirectory()}{Path.DirectorySeparatorChar}games";
+        }
+    }
+}
